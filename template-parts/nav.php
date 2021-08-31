60d00a1c829122af769fe689645cf413affba64b
Rick <rickoliwer@gmail.com>
2021-08-31 09:36:32 +0200

    edit nav

---
+++ template-parts/nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+
+        <!-- logo -->
+        <div class="logo">
+            <?php 
+                the_custom_logo();
+            ?>
+        </div>
+        <!-- end logo -->
+        <div class="front-container">
+            
+            <h1 class="front-text">hey<span>.</span></h1>
+
+            <h2 class="front-first">you're <span class="front-second">invited.</span></h2>
+        </div>
+
+</nav>
