6849ce364ea290aa7723c851a518cfed40cb5d97
Rick <rickoliwer@gmail.com>
2021-08-31 09:33:30 +0200

    edit header

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+	/**
+	 * The header for our theme
+	 *
+	 * This is the template that displays all of the <head> section and everything up until <div id="content">
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+	 *
+	 * @package Bootscore
+	 */
+	
+?>
+    
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Capace</title>
+    <?php wp_head(); ?>
+    
+</head>
+
+<body <?php body_class(); ?>>
+
+<div id="to-top"></div>
+
+<div id="page" class="site">
+
+    <header id="masthead" class="site-header">
+
+        <?php
+            get_template_part('template-parts/nav');
+
+        ?>
+
+</header><!-- #masthead -->
+    
